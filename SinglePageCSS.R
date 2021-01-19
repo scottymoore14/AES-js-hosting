c27efabfc8e1ae954054e0ccc6fc20488d733668
scottymoore14 <63413688+scottymoore14@users.noreply.github.com>
2021-01-19 17:55:23 -0500

    Add files via upload

---
+++ SinglePageCSS.R
@@ -0,0 +1,599 @@
+
+'body {
+  font-family: source-sans-pro;
+  background-color: #3E3E3E;
+    margin-top: 0px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  font-style: normal;
+  font-weight: 200;
+}
+/* Container */
+  .container {
+    width: 100%;
+    margin-left: 15px;
+    margin-right: 15px;
+    height: auto;
+    background-color: #4F4F4F;
+      max-width: 1500px;
+  }
+.main-container{
+  max-width:1500px;
+}
+
+/* Navigation */
+  header {
+    width: 100%;
+    height: 70px;
+    background-color: #4F4F4F;
+  }
+.logo {
+  color: #fff;
+    font-weight: 300;
+  text-align: undefined;
+  width: 30%;
+  float: left;
+  margin-top: 25px;
+  margin-left: 25px;
+  letter-spacing: 4px;
+}
+nav {
+  float: right;
+  width: 50%;
+  text-align: right;
+  margin-right: 25px;
+}
+header nav ul {
+  list-style: none;
+  float: right;
+}
+nav ul li {
+  float: left;
+  color: #FFFFFF;
+    font-size: 14px;
+  text-align: left;
+  margin-right: 25px;
+  margin-top: 25px;
+  letter-spacing: 2px;
+  font-weight: normal;
+  transition: all 0.3s linear;
+}
+ul li a {
+  color: #FFFFFF;
+    text-decoration: none;
+}
+ul li:hover a {
+  color: #FFFFFF;
+}
+.hero_header {
+  text-align: center;
+  margin-top: 0px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  letter-spacing: 4px;
+  color: #FFFFFF;
+    font-weight: 300;
+  font-size: 36pt;
+}
+.hero_header2 {
+  text-align: center;
+  margin-top: 20px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  letter-spacing: 4px;
+  color: #FFFFFF;
+    font-weight: 300;
+  font-size: 20pt;
+}
+
+
+/* Hero Section */
+  .hero {
+    background-image: url("sunrise spring3.jpg");
+    height: 400px;
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding-top: 100px;
+    width: 102.5%;
+    margin-left: -13px;
+    
+  }
+.light {
+  font-weight: bold;
+  color: #717070;
+}
+.tagline {
+  text-align: center;
+  color: #FFFFFF;
+    margin-top: 15px;
+  font-weight: lighter;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-size: 11pt;
+}
+/* About Section */
+  .text_column {
+    width: 29%;
+    text-align: justify;
+    font-weight: lighter;
+    line-height: 25px;
+    float: left;
+    padding-left: 20px;
+    padding-right: 20px;
+    color: #A3A3A3;
+  }
+.about {
+  padding-left: 25px;
+  padding-right: 25px;
+  padding-top: 35px;
+  background-color: #FFFFEFF;
+    margin-top: 0px;
+  color: #FFFFFF;
+}
+.data_exp {
+  width: 45%;
+  float: right;
+  padding-left: 25px;
+  padding-right: 25px;
+  padding-top: 20px;
+  display: inline-block;
+  background-color: #FFFFEFF;
+    margin-top: 0px;
+  color: #FFFFFF;
+}
+.sunburst {
+  width: 50%;
+  float: left;
+  display: inline-block;
+  padding-left: 25px;
+  padding-right: 25px;
+  padding-top: 20px;
+  background-color: #FFFFEFF;
+    margin-top: 0px;
+  color: #FFFFFF;
+}
+.sunburst2 {
+  width: 50%;
+  float: right;
+  display: inline-block;
+  padding-left: 25px;
+  padding-right: 25px;
+  padding-top: 20px;
+  background-color: #FFFFEFF;
+    margin-top: 0px;
+  color: #FFFFFF;
+}
+
+.sunburst3 {
+  width: 33%;
+  height: 240px;
+  float: left;
+  display: inline-block;
+  margin-top: 10px;
+  color: #FFFFFF;
+}
+/* Stats Gallery */
+  .stats {
+    color: #FFFFFF;
+      margin-bottom: 5px;
+    font-size: 22pt;
+  }
+.gallery {
+  clear: both;
+  display: inline-block;
+  width: 105%;
+  background-color: #9a9ba5;
+    padding-bottom: 0px;
+  padding-top: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-left: -25px
+}
+.thumbnail {
+  width: 25%;
+  text-align: center;
+  float: left;
+  margin-top: 15px;
+  border: none;
+  background-color: #9a9ba5;
+}
+.gallery .thumbnail h4 {
+  margin-top: 5px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  margin-left: 5px;
+  color: #FFFFFF;
+    font-size: 15pt;
+}
+.gallery .thumbnail p {
+  margin-top: 0px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  color: #A3A3A3;
+}
+.gallery2 {
+  clear: both;
+  display: inline-block;
+  width: 100%;
+  background-color: #41787B;
+    padding-bottom: 0px;
+  padding-top: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-left: -25px
+}
+.gallery3 {
+  clear: both;
+  display: inline-block;
+  width: 100%;
+  background-color: #RRGGBBAA
+    padding-bottom: 0px;
+  padding-top: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-left: -25px
+}
+.thumbnail2 {
+  width: 50%;
+  text-align: center;
+  float: left;
+  margin-top: 15px;
+  border: none;
+  background-color: #41787B;
+    color: #FFFFFF;
+}
+.thumbnail3 {
+  width: 100%;
+  text-align: center;
+  float: left;
+  margin-top: 15px;
+  border: none;
+  background-color:  #RRGGBBAA;
+    color: #FFFFFF;
+}
+.thumbnail4 {
+  width: 20%;
+  text-align: center;
+  float: left;
+  margin-top: 15px;
+  border: none;
+  background-color:  #RRGGBBAA;
+    color: #FFFFFF;
+}
+/* Parallax Section */
+  .banner {
+    background-image: url("sunrise spring4.jpg");
+    height: 300px;
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+.parallax {
+  color: #FFFFFF;
+    text-align: right;
+  padding-right: 100px;
+  padding-top: 110px;
+  letter-spacing: 2px;
+  margin-top: 0px;
+}
+.parallax_description {
+  color: #FFFFFF;
+    text-align: right;
+  padding-right: 100px;
+  width: 30%;
+  float: right;
+  font-weight: lighter;
+  line-height: 23px;
+  margin-top: 0px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+}
+/* More info */
+  footer {
+    background-color: #FFFFFF;
+      padding-bottom: 35px;
+  }
+.footer_column {
+  width: 50%;
+  text-align: center;
+  padding-top: 30px;
+  float: left;
+}
+footer .footer_column h3 {
+  color: #B3B3B3;
+    text-align: center;
+}
+footer .footer_column p {
+  color: #717070;
+    background-color: #FFFFFF;
+}
+.cards {
+  width: 100%;
+  height: auto;
+  max-width: 400px;
+  max-height: 200px;
+}
+footer .footer_column p {
+  padding-left: 30px;
+  padding-right: 30px;
+  text-align: justify;
+  line-height: 25px;
+  font-weight: lighter;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.button {
+  width: 300px;
+  margin-top: 20px;
+  margin-right: auto;
+  margin-bottom: auto;
+  margin-left: auto;
+  padding-top: 20px;
+  padding-right: 10px;
+  padding-bottom: 20px;
+  padding-left: 10px;
+  text-align: center;
+  vertical-align: middle;
+  border-radius: 0px;
+  text-transform: uppercase;
+  font-weight: 400;
+  font-size: 11pt;
+  letter-spacing: 2px;
+  border: 3px solid #FFFFFF;
+  color: #FFFFFF;
+    transition: all 0.3s linear;
+}
+.button:hover {
+  background-color: #FEFEFE;
+    color: #FFFFFF;
+    cursor: pointer;
+}
+.copyright {
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #717E070;
+    color: #FFFFFF;
+    text-transform: uppercase;
+  font-weight: lighter;
+  letter-spacing: 2px;
+  border-top-width: 2px;
+}
+.footer_banner {
+  background-color: #B3B3B3;
+    padding-top: 60px;
+  padding-bottom: 60PX;
+  margin-bottom: 0px;
+  
+  background-repeat: repeat;
+}
+footer {
+  display: inline-block;
+}
+.hidden {
+  display: none;
+}
+
+/* Mobile */
+  @media (max-width: 320px) {
+    .logo {
+      width: 100%;
+      text-align: center;
+      margin-top: 13px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+    }
+    .container header nav {
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      width: 100%;
+      float: none;
+      display: none;
+    }
+    header nav ul {
+    }
+    nav ul li {
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      width: 100%;
+      text-align: center;
+    }
+    .text_column {
+      width: 100%;
+      text-align: justify;
+      padding-top: 0px;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+    }
+    .thumbnail {
+      width: 100%;
+    }
+    .footer_column {
+      width: 100%;
+      margin-top: 0px;
+    }
+    .parallax {
+      text-align: center;
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      padding-top: 40%;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      width: 100%;
+      font-size: 18px;
+    }
+    .parallax_description {
+      padding-top: 0px;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      width: 90%;
+      margin-top: 25px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 12px;
+      float: none;
+      text-align: center;
+    }
+    .banner {
+      background-color: #2D9AB7;
+        background-image: none;
+    }
+    .tagline {
+      margin-top: 20px;
+      line-height: 22px;
+    }
+    .hero_header {
+      padding-left: 10px;
+      padding-right: 10px;
+      line-height: 22px;
+      text-align: center;
+    }
+  }
+
+/* Small Tablets */
+  @media (min-width: 321px)and (max-width: 767px) {
+    .logo {
+      width: 100%;
+      text-align: center;
+      margin-top: 13px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      color: #043745;
+    }
+    .container header nav {
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      width: 100%;
+      float: none;
+      overflow: auto;
+      display: inline-block;
+      background: #52bad5;
+    }
+    header nav ul {
+      padding: 0px;
+      float: none;
+    }
+    nav ul li {
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      width: 100%;
+      text-align: center;
+      padding-top: 8px;
+      padding-bottom: 8px;
+    }
+    .text_column {
+      width: 100%;
+      text-align: left;
+      padding-top: 0px;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+    }
+    .thumbnail {
+      width: 100%;
+    }
+    .footer_column {
+      width: 100%;
+      margin-top: 0px;
+    }
+    .parallax {
+      text-align: center;
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      padding-top: 40%;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      width: 100%;
+      font-size: 18px;
+    }
+    .parallax_description {
+      padding-top: 0px;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      margin-top: 30%;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      float: none;
+      width: 100%;
+      text-align: center;
+    }
+    .thumbnail {
+      width: 50%;
+    }
+    .parallax {
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      padding-top: 20%;
+    }
+    .parallax_description {
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      width: 100%;
+      padding-top: 30px;
+    }
+    .banner {
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+    .footer_column {
+      width: 100%;
+    }
+  }
+
+/* Small Desktops */
+  @media (min-width: 768px) and (max-width: 1300px) {
+    .text_column {
+      width: 100%;
+    }
+    .thumbnail {
+      width: 50%;
+    }
+    .text_column {
+      width: 100%;
+      margin-top: 0px;
+      margin-right: 0px;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      padding-top: 0px;
+      padding-right: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+    }
+    .banner {
+      margin-top: 0px;
+      padding-top: 0px;
+    }
+  }'
