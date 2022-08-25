1e054dbc6d41ec36727a4893e1c64566ee1dc3ef
TheTB96 <tyler.barnes@va.gov>
2022-08-25 16:05:23 -0400

    Add files via upload

---
+++ CSS_2022.R
@@ -0,0 +1,321 @@
+
+'body {
+  font-family: source-sans-pro;
+  background-color: #000000;
+    margin-top: 0px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  font-style: normal;
+  font-weight: 200;
+}
+.container {
+	width: 200%;
+	margin-left: -150px;
+	margin-right: auto;
+	height: 1000px;
+	background-color: #000000;
+}
+header {
+	width: 100%;
+	height: 5%;
+}
+.logo {
+	color: #fff;
+	font-weight: bold;
+	text-align: undefined;
+	width: 10%;
+	float: left;
+	margin-top: 15px;
+	margin-left: 25px;
+	letter-spacing: 4px;
+}
+nav {
+	float: right;
+	width: 50%;
+	text-align: right;
+	margin-top: 15px;
+	margin-right: 25px;
+}
+nav {
+	float: right;
+	width: 50%;
+	text-align: right;
+	margin-right: 25px;
+}
+header nav ul {
+	list-style: none;
+	float: right;
+}
+nav ul li {
+	float: left;
+	color: #FFFFFF;
+	font-size: 14px;
+	text-align: left;
+	margin-right: 25px;
+	letter-spacing: 2px;
+	font-weight: bold;
+	transition: all 0.3s linear;
+}
+ul li a {
+	color: #FFFFFF;
+	text-decoration: none;
+}
+ul li:hover a {
+  color: #FFFFFF;
+}
+.hero_header {
+  text-align: left;
+  margin-top: 100px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 50px;
+  letter-spacing: 4px;
+  color: #FFFFFF;
+  font-weight: 300;
+  font-size: 24pt;
+}
+.hero_header2 {
+  text-align: left;
+  margin-top: 20px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 50px;
+  letter-spacing: 4px;
+  color: #FFFFFF;
+  font-weight: bold;
+  font-size: 36pt;
+}
+.hero_header3 {
+  text-align: center;
+  margin-top: 20px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 50px;
+  letter-spacing: 4px;
+  color: #FFFFFF;
+  font-weight: bold;
+  font-size: 36pt;
+}
+.line1{
+	width: 20%;
+	height: 2px;
+	display: inline-block;
+	margin-left: 50px;	
+	margin-top: 15px;
+	background-color: #d4bda7;
+	border: none;
+}
+.hero {
+    background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/crumple_long.jpg");
+    height: 420px;
+    width: 120%;
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding-top: 0px;
+    margin-left: -5%;
+}
+.about {
+	background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/green gradient_long.jpg");
+	width: 120%;
+	height: 270px;
+	display: inline-block;
+	background-repeat: no-repeat;
+	margin-left: -5%;
+}
+.about1 {
+  display: inline-block;
+  width: 30%;
+  margin-top: 50px;
+  float: left;
+  margin-left: 25px;
+}
+.about2 {
+  display: inline-block;
+  width: 55%;
+  height: 100px;
+  float: right;
+	margin-top: 57px;
+	margin-right: 30px;
+  color: #FFFFFF;
+}
+.column_header {
+	margin-left: 40px;
+	margin-top: 0px;
+	margin-bottom: -10px;
+}
+.glance {
+  background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/green gradient_long.jpg");
+	width: 120%;
+	height: 90%;
+	display: inline-block;
+	margin-left: -5%;
+	margin-top: -5px;
+}
+.gallery {
+	clear: both;
+	display: flex;
+	background-color: rgba(255,255,255,0.15);
+	margin-left: 65px;
+	padding-bottom: 25px;
+	padding-top: 0px;
+	margin-top: 0px;
+	margin-bottom: 40px;
+	margin-right: 55px;
+}
+.gallery3{
+clear: both;
+    display: inline-block;
+    width: 100%;
+    background-color: #RRGGBBAA;
+    padding-bottom: 0px;
+    padding-top: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    margin-left: -25px;
+}
+.stats {
+	color: #fff;
+	display: inline-block;
+	margin-top: 15px;
+	font-size:55px;
+	font-weight: 500;
+	margin-left:30px;
+}
+.change {
+  color: #fff;
+  margin-left : 5px;
+  font-size: 20px;
+}
+.glance_type{
+  display: inline-block;
+  margin-top: -30px;
+  margin-left: 30px;
+  color: #fff;
+  font-size: 17px;
+}
+
+.thumbnail1 {
+  display: inline-block;
+  padding: 4px;
+  margin-bottom: 20px;
+  margin-left: 20px
+}
+.thumbnail4 {
+    width: 20%;
+    text-align: center;
+    float: left;
+    margin-top: 15px;
+    border: none;
+    background-color: #RRGGBBAA;
+    color: #FFFFFF;
+}
+.js-plotly-plot {
+  display: inline-block;
+  float: right;
+  margin-right: 150px;
+  margin-top: -23px;
+  margin-left: 50px;
+  margin-bottom: -30px;
+}
+.data_info {
+  margin-left: 80px;
+  width: 50%;
+}
+.comparison {
+  background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/green gradient_long.jpg");
+  width: 120%;
+  margin-left: -5%;
+}
+.deeper {
+  background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/green gradient_long.jpg");
+  width: 120%;
+  margin-left: -5%;
+  margin-top: -25px;
+}
+.banner {
+  background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/crumple.jpg");
+  width: 120%;
+  height: 30%;
+  margin-left: -5%;
+  margin-top: -2%;
+}
+.card-header {
+background-color: #526b65  !important;
+}
+.card-body {
+background-color: #526b65  !important;
+}
+.sunburst {
+  width: 50%;
+  float: left;
+  display: inline-block;
+  padding-left: 25px;
+  padding-top: 20px;
+  background-color: #FFFFEFF;
+    margin-top: 0px;
+  color: #FFFFFF;
+}
+.sunburst3 {
+  width: 33%;
+  height: 240px;
+  float: left;
+  display: inline-block;
+  margin-top: 10px;
+  color: #FFFFFF;
+}
+.thumbnail3 {
+background-color:#526b65  !important;
+height: 900px;
+}
+.button {
+  width: 300px;
+  margin-top: 20px;
+  margin-right: auto;
+  margin-bottom: auto;
+  margin-left: auto;
+  padding-top: 20px;
+  padding-right: 10px;
+  padding-bottom: 20px;
+  padding-left: 10px;
+  text-align: center;
+  vertical-align: middle;
+  border-radius: 0px;
+  text-transform: uppercase;
+  font-weight: 400;
+  font-size: 11pt;
+  letter-spacing: 2px;
+  border: 3px solid #FFFFFF;
+  color: #FFFFFF;
+    transition: all 0.3s linear;
+}
+.button:hover {
+  background-color: #FEFEFE;
+    color: #FFFFFF;
+    cursor: pointer;
+}
+.copyright {
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-image: url("https://cdn.jsdelivr.net/gh/scottymoore14/AES-js-hosting/crumple.jpg");
+  width: 120%;
+    color: #FFFFFF;
+    text-transform: uppercase;
+  font-weight: lighter;
+  letter-spacing: 2px;
+  border-top-width: 2px;
+  margin-left: -5%;
+}
+.myDiv3 {
+  height: 500px;
+}
+# /* hiders to toggle content if group not selected or if lt5 group selected */
+.hidden {
+  display: none;
+}
+#collapseOne5 > div{
+  height: 400px;
+  }
+'
